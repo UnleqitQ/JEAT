f8ca472f02d32a949ca463b3d205f0d61be6fdd7
UnleqitQ <quentin.weinbrenner@gmail.com>
2024-04-08 12:53:21 +0200

    Create Bias Node Gene

--- src/main/java/com/unleqitq/jeat/genetics/gene/node/bias/BiasNodeGene.java
+++ src/main/java/com/unleqitq/jeat/genetics/gene/node/bias/BiasNodeGene.java
@@ -1,4 +1,24 @@
 package com.unleqitq.jeat.genetics.gene.node.bias;
 
-public class BiasNodeGene {
+import com.unleqitq.jeat.genetics.gene.node.AbstractNodeGene;
+import com.unleqitq.jeat.genetics.genome.Genome;
+import lombok.Getter;
+import lombok.Setter;
+import lombok.experimental.Accessors;
+import org.jetbrains.annotations.NotNull;
+
+@Accessors(fluent = true)
+@Getter
+@Setter
+public class BiasNodeGene extends AbstractNodeGene<BiasNodeGene, BiasNodeGeneDefinition> {
+	
+	public BiasNodeGene(@NotNull Genome genome, @NotNull BiasNodeGeneDefinition definition) {
+		super(genome, definition);
+	}
+	
+	@Override
+	public @NotNull BiasNodeGene copy(@NotNull Genome genome) {
+		return new BiasNodeGene(genome, definition());
+	}
+	
 }
--- src/main/java/com/unleqitq/jeat/genetics/gene/node/bias/BiasNodeGeneDefinition.java
+++ src/main/java/com/unleqitq/jeat/genetics/gene/node/bias/BiasNodeGeneDefinition.java
@@ -1,4 +1,32 @@
 package com.unleqitq.jeat.genetics.gene.node.bias;
 
-public class BiasNodeGeneDefinition {
+import com.unleqitq.jeat.genetics.gene.node.AbstractNodeGeneDefinition;
+import com.unleqitq.jeat.genetics.genome.Genome;
+import lombok.Getter;
+import lombok.Setter;
+import lombok.experimental.Accessors;
+import org.jetbrains.annotations.NotNull;
+import org.jetbrains.annotations.Nullable;
+
+import java.util.UUID;
+
+@Accessors (fluent = true)
+@Getter
+@Setter
+public class BiasNodeGeneDefinition
+	extends AbstractNodeGeneDefinition<BiasNodeGeneDefinition, BiasNodeGene> {
+	
+	protected BiasNodeGeneDefinition(@NotNull UUID id, double x, @Nullable String name) {
+		super(id, x, true, name);
+	}
+	
+	protected BiasNodeGeneDefinition(double x, @Nullable String name) {
+		super(x, true, name);
+	}
+	
+	@Override
+	public @NotNull BiasNodeGene createGene(@NotNull Genome genome) {
+		return new BiasNodeGene(genome, this);
+	}
+	
 }
