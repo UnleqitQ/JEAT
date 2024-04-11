ad4ad82e5d20a494cc72095d186b6cbe909e97c2
UnleqitQ <quentin.weinbrenner@gmail.com>
2024-04-11 14:15:52 +0200

    Add Parameters to the leaky ReLU function

--- src/main/java/com/unleqitq/jeat/activationFunction/functions/LeakyReLUActivationFunction.java
+++ src/main/java/com/unleqitq/jeat/activationFunction/functions/LeakyReLUActivationFunction.java
@@ -1,12 +1,28 @@
 package com.unleqitq.jeat.activationFunction.functions;
 
 import com.unleqitq.jeat.activationFunction.ActivationFunction;
+import org.jetbrains.annotations.NotNull;
+
+import java.util.Map;
+import java.util.Set;
 
 public class LeakyReLUActivationFunction implements ActivationFunction {
 	
 	@Override
-	public double calculate(double value) {
-		return value > 0 ? value : 0.01 * value;
+	public double calculate(double value, @NotNull Map<String, IParameter> parameters) {
+		double alpha = parameters.get("alpha").value();
+		return value > 0 ? value : value * Math.pow(10, alpha);
+	}
+	
+	@Override
+	@NotNull
+	public Set<IParameterDefinition> parameters() {
+		return Set.of(SimpleParameterDefinition.builder()
+			.name("alpha")
+			.minValue(-5)
+			.maxValue(0)
+			.defaultValue(-2)
+			.build());
 	}
 	
 }
