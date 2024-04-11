a23daaa89165de95fc2647b9d5a52a5fef8296fd
UnleqitQ <quentin.weinbrenner@gmail.com>
2024-04-11 14:07:43 +0200

    Add Parameters to the clamped function

--- src/main/java/com/unleqitq/jeat/activationFunction/functions/ClampedActivationFunction.java
+++ src/main/java/com/unleqitq/jeat/activationFunction/functions/ClampedActivationFunction.java
@@ -1,12 +1,24 @@
 package com.unleqitq.jeat.activationFunction.functions;
 
 import com.unleqitq.jeat.activationFunction.ActivationFunction;
+import org.jetbrains.annotations.NotNull;
+
+import java.util.Map;
+import java.util.Set;
 
 public class ClampedActivationFunction implements ActivationFunction {
 	
 	@Override
-	public double calculate(double value) {
-		return Math.clamp(value, -1, 1);
+	public double calculate(double value, @NotNull Map<String, IParameter> parameters) {
+		double clampRange = parameters.get("range").value();
+		return Math.clamp(value, -clampRange, clampRange);
+	}
+	
+	@Override
+	@NotNull
+	public Set<IParameterDefinition> parameters() {
+		return Set.of(
+			SimpleParameterDefinition.builder().minValue(0.1).maxValue(10).defaultValue(1.0).name("range").build());
 	}
 	
 }
