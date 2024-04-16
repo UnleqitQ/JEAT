d81b34d6961f29f08a4120485f3862d46a2a39a0
UnleqitQ <quentin.weinbrenner@gmail.com>
2024-04-16 20:32:28 +0200

    Add method for loading in JeatConfig

--- src/main/java/com/unleqitq/jeat/config/JeatConfig.java
+++ src/main/java/com/unleqitq/jeat/config/JeatConfig.java
@@ -1,7 +1,14 @@
 package com.unleqitq.jeat.config;
 
+import com.unleqitq.jeat.config.loader.xml.JeatXmlConfigLoader;
+import com.unleqitq.jeat.genetics.genome.Genome;
 import lombok.Builder;
 import org.jetbrains.annotations.NotNull;
+
+import javax.xml.bind.JAXBException;
+import java.io.File;
+import java.util.Collection;
+import java.util.function.ToDoubleFunction;
 
 @Builder
 public class JeatConfig {
@@ -41,5 +48,43 @@ public class JeatConfig {
 	 */
 	@NotNull
 	public StagnationConfig stagnation;
+	
+	/**
+	 * Loads the configuration from an XML file.
+	 * @param file The file to load the configuration from.
+	 * @param stagnationFitnessFunction The function to calculate the fitness of a species.
+	 *                                  For more information, see {@link StagnationConfig#fitnessFunction}
+	 * @return The configuration loaded from the file.
+	 * @throws RuntimeException If an error occurs while loading the configuration.
+	 */
+	@NotNull
+	public static JeatConfig fromXml(@NotNull File file,
+		@NotNull ToDoubleFunction<Collection<Genome>> stagnationFitnessFunction) {
+		try {
+			return JeatXmlConfigLoader.load(file, stagnationFitnessFunction);
+		}
+		catch (JAXBException e) {
+			throw new RuntimeException(e);
+		}
+	}
+	
+	/**
+	 * Loads the configuration from XML.
+	 * @param content The content of the file to load the configuration from.
+	 * @param stagnationFitnessFunction The function to calculate the fitness of a species.
+	 *                                  For more information, see {@link StagnationConfig#fitnessFunction}
+	 * @return The configuration loaded from the file.
+	 * @throws RuntimeException If an error occurs while loading the configuration.
+	 */
+	@NotNull
+	public static JeatConfig fromXml(@NotNull String content,
+		@NotNull ToDoubleFunction<Collection<Genome>> stagnationFitnessFunction) {
+		try {
+			return JeatXmlConfigLoader.load(content, stagnationFitnessFunction);
+		}
+		catch (JAXBException e) {
+			throw new RuntimeException(e);
+		}
+	}
 	
 }
